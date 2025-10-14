46d9153112d43f4cfd1932045d3f62212c1bf98d
Vipul <vipulrana4546@gmail.com>
2025-10-14 15:17:52 +0530

    dom.html

---
+++ javascript/ES6/dom.js
@@ -0,0 +1,6 @@
+const div=document.createElement("div");
+const h1=document.createElement("h1");
+h1.textContent="Hello World";
+div.appendChild(h1);
+const id = document.getElementById("root");
+id.appendChild(div);
